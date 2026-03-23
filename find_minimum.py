9572e18eb2dfc7e7c51b9d0744bedb1d5e87a771
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2026-03-23 07:08:41 +0000

    Add find minimum func

--- find_minimum.py
+++ find_minimum.py
@@ -1,3 +1,14 @@
+nums = []
+
 def find_minimum(nums):
-    pass
-    
+    minimum = float("inf")
+    if len(nums) == 0:
+        return
+    for num in nums:
+        if num < minimum:
+            minimum = num
+    return minimum
+
+result = find_minimum(nums)
+
+print(result)
