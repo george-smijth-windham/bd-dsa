721abfc86e0be478f8dfeeee66ae940f04a2d308
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2026-03-25 07:04:05 +0000

    Add social spread func

--- get_estimated_spread.py
+++ get_estimated_spread.py
@@ -1,2 +1,12 @@
+from functools import reduce
+
+
 def get_estimated_spread(audiences_followers):
-    pass
+    if len(audiences_followers) == 0:
+        return 0
+    num_followers = len(audiences_followers)
+    average_audience_followers = (
+        reduce(lambda a, b: a + b, audiences_followers, 0) / num_followers
+    )
+    estimated_spread = average_audience_followers * (num_followers**1.2)
+    return estimated_spread
