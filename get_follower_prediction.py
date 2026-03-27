cf1ddd24d020da1ec5fc085b8ed47687f6560619
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2026-03-27 07:54:48 +0000

    First attempt

--- get_follower_prediction.py
+++ get_follower_prediction.py
@@ -1,2 +1,9 @@
 def get_follower_prediction(follower_count, influencer_type, num_months):
-    pass
+    # follower_count * (follower_type^num_months)
+    initial_follower_count = follower_count
+    constant = (
+        4 if "fitness" == influencer_type else 3 if "cosmetic" == influencer_type else 2
+    )
+    predicted_follower_count = initial_follower_count * (constant ^ num_months)
+    print(f"{influencer_type} constant: {constant}")
+    return
