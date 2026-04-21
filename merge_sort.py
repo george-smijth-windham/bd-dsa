bf763f45041cc3756768e9cd0f4ee2c17b0b85ad
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2026-04-21 07:49:39 +0100

    Save work

--- merge_sort.py
+++ merge_sort.py
@@ -1,28 +1,14 @@
 def merge_sort(nums):
+    # nums = [1,2]
+    # print(f"nums: {nums}", f"left_side: {nums[:len(nums) // 2]}", f"right_side: {nums[len(nums) // 2:]}")
+    # return []
     if len(nums) < 2:
         return nums
-    sorted_left_side = merge_sort(nums[: len(nums) // 2])
-    sorted_right_side = merge_sort(nums[len(nums) // 2 :])
-    return merge(sorted_left_side, sorted_right_side)
+    left_side = merge_sort(nums[: len(nums) // 2])
+    right_side = merge_sort(nums[len(nums) // 2 :])
+    print(f"nums: {nums}", f"left_side: {left_side}", f"right_side: {right_side}")
+    return merge(left_side, right_side)
 
 
-def merge(sorted_left_side, sorted_right_side):
-    pass
-    # left_index, right_index = 0, 0
-    # sorted = []
-    # print(
-    #     f"len_left: {len(sorted_left_side)}",
-    #     f"sorted_left_side: {sorted_left_side}",
-    #     f"len_right: {len(sorted_left_side)}",
-    #     f"sorted_right_side: {sorted_right_side}",
-    #     sep="\n",
-    # )
-    # while left_index < len(sorted_left_side) or right_index < len(sorted_right_side):
-    #     if sorted_left_side[left_index] <= sorted_right_side[right_index]:
-    #         print(
-    #             f"left lt or eq right: {sorted_left_side[left_index]}, {sorted_right_side[right_index]}"
-    #         )
-    #         left_index += 1
-    #     else:
-    #         right_index += 1
-    # return sorted_left_side + sorted_right_side
+def merge(left_side, right_side):
+    return []
