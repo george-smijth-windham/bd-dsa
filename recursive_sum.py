a868cb072c64131f6e7d5e063e4d414f7aa34769
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2026-04-25 18:26:23 +0100

    Update recursive sum func

--- recursive_sum.py
+++ recursive_sum.py
@@ -1,6 +1,6 @@
-def recursive_sum(numbers):
-    return 0 if len(numbers) == 0 else numbers[0] + recursive_sum(numbers[1:])
+def recursive_sum_list(numbers):
+    return 0 if len(numbers) == 0 else numbers[0] + recursive_sum_list(numbers[1:])
 
 
-def recursive_sum(numbers):
-    return 0 if len(numbers) == 0 else numbers[0] + recursive_sum(numbers[1:])
+def recursive_sum_n(n):
+    return 0 if n <= 0 else n + recursive_sum_n(n - 1)
