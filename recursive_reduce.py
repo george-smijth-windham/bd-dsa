b5e2774a4062ba0a4d4a49996cb821f86c040bd9
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2026-04-30 12:00:26 +0100

    Add comment recursive reduce fn

--- recursive_reduce.py
+++ recursive_reduce.py
@@ -4,3 +4,16 @@ def recursive_reduce(values, fn, initial):
         if len(values) == 0
         else fn(values[0], recursive_reduce(values[1:], fn, initial))
     )
+
+
+# a way to deal with missing initial
+# _MISSING = object()
+
+# def recursive_reduce(values, fn, initial=_MISSING):
+# if initial is _MISSING:
+# if len(values) == 0:
+# raise ValueError("reduce of empty sequence with no initial value")
+# if len(values) == 1:
+# return values[0]
+# proceed without initial...
+# ...
