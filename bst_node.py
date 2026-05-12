c334925cb5d82583fc101c85ef912ca689963e27
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2026-05-12 14:06:24 +0100

    Update bst node ds

--- bst_node.py
+++ bst_node.py
@@ -1,30 +1,60 @@
 class BSTNode:
+    def delete(self, val):
+        if self.val is None:
+            return None
+        if val < self.val:
+            if self.left is not None:
+                self.left = self.left.delete(val)
+                return self
+        if val > self.val:
+            if self.right is not None:
+                self.right = self.right.delete(val)
+                return self
+        if self.left is None:
+            return self.right
+        if self.right is None:
+            return self.left
+        right = self.right
+        successor = right.get_min()
+        self.val = successor
+        self.right = self.right.delete(successor)
+        return self
+
+    # don't touch below this line
+
     def __init__(self, val=None):
         self.left = None
         self.right = None
         self.val = val
 
     def insert(self, val):
-        if self.val is None:
+        if not self.val:
             self.val = val
             return
+
         if self.val == val:
             return
-        if val < self.val and self.left is None:
+
+        if val < self.val:
+            if self.left:
+                self.left.insert(val)
+                return
             self.left = BSTNode(val)
             return
-        if val < self.val and self.left is not None:
-            self.left.insert(val)
-            return
-        if val > self.val and self.right is None:
-            self.right = BSTNode(val)
-            return
-        if val > self.val and self.right is not None:
+
+        if self.right:
             self.right.insert(val)
             return
+        self.right = BSTNode(val)
 
     def get_min(self):
-        return self.val if self.left is None else self.left.get_min()
+        current = self
+        while current.left is not None:
+            current = current.left
+        return current.val
 
     def get_max(self):
-        return self.val if self.right is None else self.right.get_max()
+        current = self
+        while current.right is not None:
+            current = current.right
+        return current.val
