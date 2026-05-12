29fbe46dc8465d18d91a980579346daf910decc9
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2026-05-12 10:26:39 +0100

    Update bst node ds

---
+++ bst_node.py
@@ -0,0 +1,24 @@
+class BSTNode:
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val
+
+    def insert(self, val):
+        if self.val is None:
+            self.val = val
+            return
+        if self.val == val:
+            return
+        if val < self.val and self.left is None:
+            self.left = BSTNode(val)
+            return
+        if val < self.val and self.left is not None:
+            self.left.insert(val)
+            return
+        if val > self.val and self.right is None:
+            self.right = BSTNode(val)
+            return
+        if val > self.val and self.right is not None:
+            self.right.insert(val)
+            return
