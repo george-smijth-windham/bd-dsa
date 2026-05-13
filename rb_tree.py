d3e7894384f41824b8c65914d3e69a8d8f4ae54a
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2026-05-13 17:26:11 +0100

    Update rb tree ds

--- rb_tree.py
+++ rb_tree.py
@@ -10,4 +10,28 @@ class RBTree:
         self.root = self.nil
 
     def insert(self, val):
-        pass
+        new_node = RBNode(val)
+        new_node.parent = None
+        new_node.left = self.nil
+        new_node.right = self.nil
+        new_node.red = True
+
+        parent = None
+        current = self.root
+        while current != self.nil:
+            parent = current
+            if new_node.val < current.val:
+                current = current.left
+            elif new_node.val > current.val:
+                current = current.right
+            else:
+                # duplicate, just ignore
+                return
+
+        new_node.parent = parent
+        if parent is None:
+            self.root = new_node
+        elif new_node.val < parent.val:
+            parent.left = new_node
+        else:
+            parent.right = new_node
