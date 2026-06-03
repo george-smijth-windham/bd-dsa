9f05429dab201442c014f9dbbf2c26390e966caa
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2026-06-03 17:10:19 +0100

    Update hash map ds

--- hash_map.py
+++ hash_map.py
@@ -1,10 +1,29 @@
 from typing import Any
-
-
 class HashMap:
     def insert(self, key: str, value: Any) -> None:
-        index = self.key_to_index(key)
+        original_index = index = self.key_to_index(key)
+        first_iteration = True
+        while self.hashmap[index] is not None and self.hashmap[index][0] != key:
+            if first_iteration == False and index == original_index:
+                raise Exception("hashmap is full")
+            index = (index + 1) % len(self.hashmap)
+            first_iteration = False
         self.hashmap[index] = key, value
+
+
+    def get(self, key: str) -> Any:
+        original_index = index = self.key_to_index(key)
+        first_iteration = True
+        while self.hashmap[index] is not None:
+            k, v = self.hashmap[index]
+            if k == key:
+                return v
+            if first_iteration is False and index == original_index:
+                raise Exception("sorry, key not found")
+            index = (index + 1) % len(self.hashmap)
+            first_iteration = False
+
+
 
     # don't touch below this line
 
@@ -21,7 +40,5 @@ class HashMap:
         final = ""
         for i, v in enumerate(self.hashmap):
             if v != None:
-                final += f" - {i}: {str(v)}\n"
-            else:
-                final += f" - {i}: None\n"
+                final += f" - {str(v)}\n"
         return final
